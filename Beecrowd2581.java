2f4a80485c4d36c6019f8988b53c03d3505a4845
Henrique-Kisaki <satoshikisaki@hotmail.com>
2023-06-04 23:51:07 -0300

    Beecrowd - Exercício 2581 (Categoria: Iniciante)

---
+++ Beecrowd2581.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Beecrowd2581 {
+
+    public static void main (String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        for(int i = 0; i < N; i++) {
+            if(sc.hasNext()) {
+                String curso = sc.nextLine();
+            }
+            System.out.println("I am Toorg!");
+        }
+
+        sc.close();
+
+    }
+    
+}
